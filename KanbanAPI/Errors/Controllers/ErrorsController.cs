f6e4c1614d624a3cbd6b0d1941697449d36c5d4f
Kalvin <kalvinhart@hotmail.co.uk>
2024-05-15 22:41:20 +0100

    added global error handling

---
+++ KanbanAPI/Errors/Controllers/ErrorsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KanbanAPI.Errors.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ErrorsController : ControllerBase
+{
+    [Route("")]
+    public IActionResult Error()
+    {
+        Exception? exception = HttpContext.Features.Get<IExceptionHandlerFeature>()?.Error;
+
+        return exception != null
+            ? Problem(title: exception.Message)
+            : Problem();
+    }
+}
--- KanbanAPI/Program.cs
+++ KanbanAPI/Program.cs
@@ -34,6 +34,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseExceptionHandler("/api/errors");
+
 app.UseAuthorization();
 
 app.MapControllers();
